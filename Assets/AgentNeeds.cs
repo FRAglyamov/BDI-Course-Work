0eb25218fba29e6c7affc7282aa6285a46bf3920
lucklessfox <forick007@gmail.com>
2018-10-30 10:38:21 +0300

    upd

--- Assets/AgentNeeds.cs
+++ Assets/AgentNeeds.cs
@@ -4,7 +4,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[Serializable]
+public class Desire
+{
+    public string name;
+    public float value;
+    public string solverIntention;
+}
+[Serializable]
+public class Intention
+{
+    public string name;
+    public IntentionPrimitiveTypeAction primitiveAction;
+    public Func<float> calculateCostFunc;
+    public string knowledgeLabel;
+}
+public enum IntentionPrimitiveTypeAction : byte
+{
+    None,
+    GoTo,
+    TakeItem
+}
+public struct UnityBDIExecutingIntentionInfo
+{
+    public Intention intentionBase;
+    public BaseMapObject selectedMapObject;
+    public UnityBDIExecutingIntentionInfo(Intention intentionBase)
+    {
+        this.intentionBase = intentionBase;
+        this.selectedMapObject = null;
+    }
+    public UnityBDIExecutingIntentionInfo(Intention intentionBase, BaseMapObject selectedMapObject)
+    {
+        this.intentionBase = intentionBase;
+        this.selectedMapObject = selectedMapObject;
+    }
+}
+
 public class AgentNeeds : MonoBehaviour {
+
+    List<Desire> desire = new List<Desire>();
+    public Dictionary<string, int> items = new Dictionary<string, int>();
+
+    public List<Intention> intentions = new List<Intention>();
+    public Queue<UnityBDIExecutingIntentionInfo> intentionsQueue = new Queue<UnityBDIExecutingIntentionInfo>();
+    public Dictionary<string, HashSet<BaseMapObject>> knowledge_mapObjects = new Dictionary<string, HashSet<BaseMapObject>>();
 
     public float hunger = 0f;
     public float energy = 0f;
---
+++ Assets/IntentionDrawer.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+[CustomPropertyDrawer(typeof(Intention))]
+public class IntentionDrawer : PropertyDrawer {
+    /*
+    private SerializedProperty prop_name;
+    private SerializedProperty prop_nextIntentionsIDs;
+    private SerializedProperty prop_primitiveAction;
+    private SerializedProperty prop_knowledgeLabel;
+    private SerializedProperty prop_agent;
+    private int selectedStat;
+    private SerializedProperty _lastUpdatedProp;
+
+    private void Initialize(SerializedProperty property)
+    {
+        if (_lastUpdatedProp != property)
+        {
+            prop_name = property.FindPropertyRelative("name");
+            prop_nextIntentionsIDs = property.FindPropertyRelative("nextIntentionsIDs");
+            prop_primitiveAction = property.FindPropertyRelative("primitiveAction");
+            prop_knowledgeLabel = property.FindPropertyRelative("knowledgeLabel");
+            prop_agent = property.FindPropertyRelative("agent");
+            _lastUpdatedProp = property;
+        }
+    }
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        Initialize(property);
+        UnityBDIAgent agent = prop_agent.objectReferenceValue as UnityBDIAgent;
+        EditorGUI.indentLevel = 0;
+        // Box coloring by id key
+        GUI.color = Utilities.GetColorForString(prop_name.stringValue);
+        GUI.Box(new Rect(position.x, position.y, position.width, GetPropertyHeight(property, GUIContent.none)), GUIContent.none);
+        position.x += 4f;
+        position.width -= 8f;
+        position.y += 4f;
+        GUI.color = new Color(GUI.color.r * 0.1f + 0.9f, GUI.color.g * 0.1f + 0.9f, GUI.color.b * 0.1f + 0.9f);
+        position = UnityBDIEditorUtilities.DrawProperty(position, prop_name);
+        position = UnityBDIEditorUtilities.DrawProperty(position, prop_primitiveAction);
+        position = UnityBDIEditorUtilities.DrawProperty(position, prop_knowledgeLabel);
+        List<string> intentionNames = new List<string>();
+        List<Intention> intentions = agent.intentions;
+
+        for (int i = 0; i < intentions.Count; i++)
+        {
+            intentionNames.Add(intentions[i].name);
+        }
+        ReorderableList list;
+        list = new ReorderableList(property.serializedObject, prop_nextIntentionsIDs, true, true, true, true);
+        list.drawHeaderCallback += rect => GUI.Label(rect, label);
+        list.drawElementCallback += (rect, index, active, focused) =>
+        {
+            rect.height = 16; rect.y += 2; int selectedIntention = intentionNames.FindIndex(s => s.Equals(prop_nextIntentionsIDs.GetArrayElementAtIndex(index).stringValue)); if (selectedIntention < 0) { selectedIntention = 0; }
+            selectedIntention = EditorGUI.Popup(rect, selectedIntention, intentionNames.ToArray());
+            prop_nextIntentionsIDs.GetArrayElementAtIndex(index).stringValue = intentions[selectedIntention].name;
+        };
+        list.DoList(position);
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        Initialize(property);
+        return EditorGUI.GetPropertyHeight(prop_name) + 
+            EditorGUI.GetPropertyHeight(prop_primitiveAction) + 
+            EditorGUI.GetPropertyHeight(prop_knowledgeLabel) + 
+            (prop_nextIntentionsIDs.arraySize * 20f) 
+            + 3 * 4f + 62f;
+    }
+    */
+}
